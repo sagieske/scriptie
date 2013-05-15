ee76cd1db161f5e01fa8fbca404133e2c9c20fa5
sagieske <sharongieske@gmail.com>
2013-05-16 00:22:23 +0200

    unknown class count added

--- classification.py
+++ classification.py
@@ -12,23 +12,23 @@ class Main():
 	testSet = []
 
 	# Dictionary for class, classify unkown into non-activity
-	class_dict = {"Y": 0, "N": 1, "U": 1}
+	class_dict = {"Y": 0, "N": 1, "U": 2}
 
 	# Distribution of trainingset, testset, validationset
 	distribution = (0.7, 0.2, 0.1)
 
 	# Read file
 	DELIMITER = "\t"
-	data = csv.reader(open("2000test.csv", 'rU'), delimiter=DELIMITER)
+	data = csv.reader(open("2000test_annotated.csv", 'rU'), delimiter=DELIMITER)
 
 	def __init__(self):
 		"""
 		INIT
 		"""
 		# Load tweets from file
-		#self.initialize()
-		#self.count_classes()
-		#self.createSets()
+		self.initialize()
+		self.count_classes()
+		self.createSets()
 		self.stemmer()
 
 	def initialize(self):
@@ -39,9 +39,9 @@ class Main():
 		# Create tweet and class lists
 		for i, row in enumerate(self.data):
 			# Ignores header
-			#if(i != 0):
+			if(i != 0):
 			# TEMP, for testing only
-			if (i < 20):
+			#if (i < 20):
 				# Get tweet and class 
 				self.tweets[i-1] = row[3]
 				self.tweet_class[i-1] = self.class_dict.get(row[5])
@@ -50,7 +50,7 @@ class Main():
 		"""
 		Create training/test/validation set via indices 
 		"""
-		for i in range(1, 20):
+		for i in range(1, len(self.tweets)):
 			# Test if random number is smaller than distribution for trainset
 			r_nr = random.random()
 			if (r_nr < self.distribution[0]):
@@ -69,12 +69,14 @@ class Main():
 		# Count occurances of classes
 	 	activity_count = values.count(0)
 		nonactivity_count = values.count(1)
+		unknown_count = values.count(2)
 
 		# Print
 		print ">> Statistics:"
 		print "Total number of tweets: %i" % total
 		print "Total activity tweets: %i" % activity_count
 		print "Total non-activity tweets: %i" % nonactivity_count
+		print "Total unknown-activity tweets: %i" % unknown_count
 
 	def stemmer(self):
 		# USE FROG HERE? 
