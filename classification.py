2c88c3348951fbba63c0413c2b635467e9d47877
sagieske <sharongieske@gmail.com>
2013-05-15 13:36:33 +0200

    tweets and tweetclasses initialized

--- classification.py
+++ classification.py
@@ -9,13 +9,26 @@ def createSets(filename):
 	# Distribution of trainingset, testset, validationset
 	distribution = (0.7, 0.2, 0.1)
 
-	# Read file
-	DELIMITER = ","
-	data = csv.reader(open(filename, 'rU'), delimiter=DELIMITER)
-	#totaltweets = len(data)
-	print data[22]
-	#for i, row in enumerate(data):
+	# Lists for tweet and class
+	tweets = {}
+	tweet_class = {}
+	
+	# Dictionary for class, classify unkown into non-activity
+	class_dict = {"Y": 0, "N": 1, "U": 1}
 
+	# Read file
+	DELIMITER = "\t"
+	data = csv.reader(open(filename, 'rU'), delimiter=DELIMITER)
+	
+	# Create tweet and class lists
+	for i, row in enumerate(data):
+		# Ignores header
+		#if(i != 0):
+		# TEMP, for testing only
+		if (i < 20):
+			# Get tweet and class 
+			tweets[i-1] = row[3]
+			tweet_class[i-1] = class_dict.get(row[5])
 		
 
 def main(argv):
