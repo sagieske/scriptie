3718459646aef2017fcb3fee7a5091c04890fb86
sagieske <sharongieske@gmail.com>
2013-05-26 02:35:21 +0200

    class preprocessing created

--- classification.py
+++ classification.py
@@ -1,15 +1,15 @@
-import csv
-import sys
-import random
-import nltk
-import os
+import csv											# used for import csv tweets
+import random										# used for train/test set creation
+import nltk											# used for tokenization
+import os											# used for calling Frog in new terminal
 import operator
-import time
-import socket 
-from pynlpl.clients.frogclient import FrogClient
-import subprocess
-import signal
-import math
+import time											# used for timer
+from pynlpl.clients.frogclient import FrogClient	# used for Frog client
+import subprocess									# used for calling Frog in new terminal
+import signal										# used for calling Frog in new terminal
+import math	 
+import pickle										# write and load list to file
+import nltk.stem as Stemmer							# used for stemmer
 
 """
 TODO:
@@ -18,24 +18,24 @@ TODO:
 """
 
 
-class Main():
+class Main(object):
 
 	# Lists for tweet and class
 	tweets = {}
 	tweet_class = {}
 	corpus = {}
 	corpus_weights = {}
-	corpus_weights_test = {}
 	bigramcorpus = {}
 	trigramcorpus = {}
 	trainSet = []
 	testSet = []
+	bagOfWords = []
 
 	totalPos = 0
 	totalNeg = 0
 	
 	# Portnumber for Frog
-	portnumber = 1200
+	portnumber = 1150
 
 	# Dictionary for class, classify unkown into non-activity
 	class_dict = {"Y": 0, "N": 1, "U": 2}
@@ -47,38 +47,55 @@ class Main():
 	DELIMITER = "\t"
 	data = csv.reader(open("2000test_annotated.csv", 'rU'), delimiter=DELIMITER)
 
-	def __init__(self):
+	def __init__(self, mode):
 		"""
 		INIT
 		"""
-		# Load tweets from file
-		self.initialize()
-		self.count_classes()
-		self.createSets()
-		self.createCorpus("Frog")
-		self.setCorpusWeights()
-		#self.printTweetsToText()
-		#self.useFrog()
-		#self.readFrog()
-		#tweet = "eens even kijken hoe ik hier naar kijk bla &"
- 		#self.createTokens('ik kijkte naar gekeken materiaal en brave braaf zwetende zweet bah! ?',"frog lemma")
+		self.initialize(mode)
 
-	def initialize(self):
+		self.count_classes()
+		self.createCorpus(mode)
+		self.setCorpusWeights()
+		self.scaleCorpusWeights()
+
+		self.bagOfWords = self.corpus_weights.keys()
+
+
+	def initialize(self, mode):
 		"""
 		Initializes tweet and class sets
 		"""
-		print "Initialize sets.."
+		print "Initialize.."
+
+
 		# Create tweet and class lists
 		for i, row in enumerate(self.data):
 			# Ignores header
 			if(i != 0):
 			# TEMP, for testing only
-				if (i > 1000 and i < 1400):
+				begin = 1000
+				end = 1500
+				if (i >= begin and i <= end):
 					# Only add if class is known! for testing
 					if (self.class_dict.get(row[5].upper()) is not None):
 						# Get tweet and class 
-						self.tweets[i-1000] = row[3]
-						self.tweet_class[i-1000] = self.class_dict.get(row[5].upper())
+						self.tweets[i-begin] = row[3]
+						self.tweet_class[i-begin] = self.class_dict.get(row[5].upper())
+
+		modelist = mode.split()
+		# Create Train and Test
+		if (modelist[0] == "frog"):
+			self.createSets()
+
+		# DEBUG: read sets from file
+		if (modelist[0] == "frogdb"):
+			f = file("sets.txt", "r")
+			totallist = pickle.load(f)
+			self.trainSet = totallist[0]
+			self.testSet = totallist[1]
+			print self.testSet
+
+
 
 	def createSets(self):
 		"""
@@ -91,7 +108,15 @@ class Main():
 				self.trainSet.append(i)
 			else:
 				self.testSet.append(i)
+		
+		print self.testSet
 
+		# DEBUG: write to file
+		totallist = []
+		totallist.append(self.trainSet)
+		totallist.append(self.testSet)
+		f = file("sets.txt", "w")
+		pickle.dump(totallist, f)
 	
 	def count_classes(self):
 		"""
@@ -116,41 +141,60 @@ class Main():
 		"""
 		Create training corpus
 		"""
-		if(mode == "Frog"):
-			# Start Frog Server
+		modelist = mode.split()
+
+		if(modelist[0] == "frogdb"):
+			#WARNING TRAIN AND TEST SET ARE DIFFERENT, IS NOT WRITEN TO FILE! 
+			frogclient = None
+			tokenarray = []
+			for line in open('frog_tokens.txt','r'):
+				tokens = line.split("\t")
+				tokenarray.append(tokens)
+			print len(tokenarray)
+			for tokens in tokenarray:
+				tweetclass = int(tokens[len(tokens)-1])
+				del tokens[-1]
+				# add every token to corpus
+				self.addTokensToCorpus(tokens, tweetclass, 2)
+
+					
+		# Start Frog server and client
+		if(modelist[0] == "frog"):
 			self.startFrogServer('start')
-			# Wait for Frog to startup
 			time.sleep(15)
-			# Start Frog Client
 			frogclient = FrogClient('localhost',self.portnumber)
+			open("frog_tokens.txt", 'w').close()
 
-		# USE FROG HERE? 
-		# open frog to do all stemming of every sentence? sentences aan elkaar plakken, in frog gooien en weer uit elkaar halen?
-		#if(mode == "Frog"):
-		#	os.system("frog -t test.txt > frogtesting.txt")
-		for index in self.tweets:
-			tweet = self.tweets[index]	
-			tweetclass = self.tweet_class[index]		
+			print "Testing trainset size:"
+			print len(self.trainSet)
+
+			for index in self.trainSet:
+				tweet = self.tweets[index]	
+				tweetclass = self.tweet_class[index]		
 			
-			# Split into tokens	
-			tokens = self.createTokens(frogclient, tweet,"frog word")
+				# Split into tokens	
+				tokens = self.createTokens(frogclient, index, mode)
 
-			# add every token to corpus
-			for index, item in enumerate(tokens):
-				# check class
-				if(tweetclass == 0):
-					addition = (1,0)
-					self.totalPos += 1
-				else:
-					addition = (0,1)
-					self.totalNeg += 1
-				self.addToCorpus(tokens, index, addition, 3)
+				self.addTokensToCorpus(tokens, tweetclass, 2)
 
-		if(mode == "Frog"):
+		if(modelist[0] == "Frog"):
 			# Stop Server
 			self.startFrogServer('stop')
 
-	def createTokens(self,frogclient, tweet,mode):
+	def addTokensToCorpus(self,tokens,tweetclass, ngram):
+		"""
+		add every token to corpus
+		"""
+		for index, item in enumerate(tokens):
+			if(tweetclass == 0):
+				addition = (1,0)
+				self.totalPos += 1
+			else:
+				addition = (0,1)
+				self.totalNeg += 1
+			self.addToCorpus(tokens, index, addition, ngram)
+
+	def createTokens(self,frogclient, index,mode):
 		"""
 		Creates tokens for corpus dependent of mode
 		"""
@@ -159,15 +203,26 @@ class Main():
 		if modelist[0] == "tk":
 			print "tk"
 			tokens = nltk.word_tokenize(tweet)
-		if modelist[0] == "frog":
-			#print "frog"
-			# Process tweet
-	 		# TODO: test is lower
-			frogtweet = frogclient.process(tweet.lower())
-			tokens = self.processFrogtweet(tweet.lower(), frogtweet, modelist[1])
 
+		if modelist[0] == "frogdebug":
+			pass
+		if modelist[0] == "frog":
+			tweet = self.tweets[index]
+			frogtweet = frogclient.process(tweet.lower())
+			tokens = self.processFrogtweet(tweet, frogtweet, modelist[1])
+			# Write tokens to file for later testing
+			with open("frog_tokens.txt", "a") as myfile:
+				stringtokens = self.tokensToString(tokens)
+				myfile.write(stringtokens.encode('utf-8') + str(self.tweet_class[index]).encode('utf-8') + "\n".encode('utf-8'))
 		return tokens
 
+	def tokensToString(self,tokens):
+		string = ""
+		for item in tokens:
+			string += item
+			string += "\t"
+		return string
+		
 	def processFrogtweet(self, tweet, frogtweet, frogmode):	
 		"""
 		Process Frog information for requested items into token list
@@ -193,9 +248,12 @@ class Main():
 		
 
 	def startFrogServer(self, mode):
+		"""
+		Starts/stops Frog server in seperate terminal
+		"""
 		if(mode == 'start'):
 			print "start"
-			os.system("mate-terminal -e 'frog -S 1200'")
+			os.system("mate-terminal -e 'frog -S " + str(self.portnumber) + "'")
 		if(mode == 'stop'):
 			print "stop"
 			proc = subprocess.Popen(["pgrep", 'frog'], stdout=subprocess.PIPE) 
@@ -218,7 +276,7 @@ class Main():
 			if tupleItem in self.corpus:
 				self.corpus[tupleItem] = tuple(map(operator.add, self.corpus[tupleItem], (addition)))
 			else:
-				self.corpus[tupleItem] = addition	
+				self.corpus[tupleItem] = addition
 
 	def setCorpusWeights(self):
 		"""
@@ -244,42 +302,39 @@ class Main():
 
 				else:
 					counter += 1
-		self.scaleCorpusWeights()
 
 
 	def scaleCorpusWeights(self):
-		print "scaling"
-
+		"""
+		Scale weights to [-1,1]
+		"""
+		# Get old range
 		oldMax = max(self.corpus_weights.iteritems(), key=operator.itemgetter(1))[1]
 		oldMin = min(self.corpus_weights.iteritems(), key=operator.itemgetter(1))[1]
 		print "scaling from [%f,%f] to [-1,1]" %(float(oldMax),float(oldMin))
+		# Set new range
 		newMin = float(-1.0)
 		newMax = float(1.0)
 		
+		# Calculate new values
+		print "Before"
+		print len(self.corpus_weights)
+		delkeys = []
 		for key in self.corpus_weights:
 			oldValue = float(self.corpus_weights[key])
 			newValue = (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
-			self.corpus_weights[key] = newValue
-			if newValue < 0:
-				print newValue
+			if(newValue < 0.25 and newValue > -0.25):
+				delkeys.append(key)
+			else:	
+				self.corpus_weights[key] = newValue
 
-		self.findHighest(self.corpus_weights, 10)
-		self.findLowest(self.corpus_weights, 10)		
-		self.createNewCorpusWeights()
-
-	def createNewCorpusWeights(self):
-		counter =0
-		counter5 =0
-		counter6 = 0
-		for key in self.corpus_weights:
-			value =  self.corpus_weights[key]
-			if (value > 0.5 or value < -0.5):
-				self.corpus_weights_test[key] = self.corpus_weights[key]
-
+		for key in delkeys:
+			del self.corpus_weights[key]
+		print "After"
 		print len(self.corpus_weights)
-		print len(self.corpus_weights_test)
-
-		#self.corpus_weights.update({n: 2 * my_dict[n] for n in my_dict.keys()})
+		self.findHighest(self.corpus_weights, 10)
+		self.findLowest(self.corpus_weights, 10)
+		
 
 	def findHighest(self,corpus, nr):
 		"""
@@ -288,7 +343,7 @@ class Main():
 		topCorpus = dict(sorted(corpus.iteritems(), key=operator.itemgetter(1), reverse=True)[:nr])
 		for item in topCorpus:
 			value = topCorpus[item]
-			print "(%s, %s, %s) : %f" % (item[0], item[1], item[2], value)
+			print "(%s, %s) : %f" % (item[0], item[1], value)
 		#print topCorpus
 
 	def findLowest(self,corpus, nr):
@@ -298,7 +353,47 @@ class Main():
 		topCorpus = dict(sorted(corpus.iteritems(), key=operator.itemgetter(1), reverse=False)[:nr])
 		for item in topCorpus:
 			value = topCorpus[item]
-			print "(%s, %s, %s) : %f" % (item[0], item[1], item[2], value)
+			print "(%s, %s) : %f" % (item[0], item[1], value)
 		#print topCorpus
+"""
+# The docstring for a class should summarize its behavior and list the public methods and instance variables
+# preproccessing: process all tweets in particular way, several functions for severall preprocessing
+# input: Mode of preproccessing, tweets array
+"""
 
-m = Main()
+class Preprocessing(object):
+	"""
+	Class for preprocessing tweets
+	blablaballa etc
+
+	"""
+	stemmed_tweets_array = []
+
+	def __init__(self, mode, tweetarray):
+		"""	Initialize tweetarray for use"""
+		self.tweetarray = tweetarray
+		self.mode = mode
+
+	def stemming_str(self):
+		for item in self.stemmed_tweets_array:
+			print item
+
+	def stemming(self):
+		""" Stem all tweets given to object and set to array """
+		for item in self.tweetarray:
+			stemmed_tweet = self.stem_tweet(item)		
+			self.stemmed_tweets_array.append(stemmed_tweet)
+
+	def stem_tweet(self, tweet):
+		""" Stem tweet string and return array of stemmed words """
+		stemmer = Stemmer.SnowballStemmer('dutch')
+		stemmed_tweet = stemmer.stem(tweet)
+		stemmed_tweet = stemmed_tweet.split()
+		return stemmed_tweet
+		
+test = Preprocessing("test", ["Dit is een tweet die uitgeprobeerd gaat worden", "test dit testte even kijken"])
+test.stemming()
+test.stemming_str()
+
+#m = Main("frog lemma")
+#m = Main("frogdb lemma")
