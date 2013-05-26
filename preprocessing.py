4ddf3a8cbd9db51a1d10b0b8a89b709f2f13780d
sagieske <sharongieske@gmail.com>
2013-05-26 05:13:14 +0200

    frog not double used anymore

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,7 @@ import math
 import pickle										# write and load list to file
 import nltk											# used for stemmer
 import re
+import time
 
 class Preprocessing(object):
 	"""
@@ -15,11 +16,17 @@ class Preprocessing(object):
 	blablaballa etc
 
 	"""
-	portnumber = 1150	# Portnumber for Frog
+	# Debug files
+	DEBUG_STEM = "debug_stem.txt"
+	DEBUG_TOKEN = "debug_token.txt"
+	DEBUG_LEMMA = "debug_lemma.txt"
+	DEBUG_POS = "debug_pos.txt"
+	PORTNUMBER = 1150	# PORTNUMBER for Frog
 
 	stemmed_tweets_array = []
 	tokenized_tweets_array = []
 	lemmatized_tweets_array = []
+	pos_tweets_array = []
 
 	def __init__(self, mode, tweetarray):
 		"""	Initialize tweetarray for use"""
@@ -33,41 +40,51 @@ class Preprocessing(object):
 
 	def preprocess_tweets(self):
 		mode_args = self.mode.split()
-		if(mode_args[0] == "stem"):
+		if("stem" in mode_args):
 			self.stemming()
 			self.stemming_str()
-		if(mode_args[0] == "token"):
+		if("token" in mode_args):
 			self.tokenize()
 			self.tokenize_str()
-		if(mode_args[0] == "frog"):
-			self.frogtokens(mode_args[1])
-			self.frogtokens_str()
+		if("frog" in mode_args):
+			self.frogtokens()
+			if("lemma" in mode_args):
+				self.lemmatized_str()
+			if("pos" in mode_args):
+				self.pos_str()
 				
 	def stemming_str(self):
-		print "** Stemmed:\n"
+		print ">> Stemmed:"
 		for item in self.stemmed_tweets_array:
-			print '|'.join(item)
+			print '- ' + '|'.join(item)
 
 	def tokenize_str(self):
-		print "** Tokenized: \n"
+		print ">> Tokenized:"
 		for item in self.tokenized_tweets_array:
-			print '|'.join(item)
+			print '- ' + '|'.join(item)
 
-	def frogtokens_str(self):
-		print "** Lemmatized: \n"
+	def lemmatized_str(self):
+		print ">> Lemmatized:"
 		for item in self.lemmatized_tweets_array:
-			print '|'.join(item)
+			print '- ' + '|'.join(item)
+
+	def pos_str(self):
+		print ">> POS tagged:"
+		for item in self.pos_tweets_array:
+			print '- ' + '|'.join(item)
 
 	def stemming(self):
 		""" Stem all tweets given to object and set to array """
-		if (self.debug):
+		print "** Stemming.."
+		debug = self.debug
+		if (debug):
 			try:
-				self.read_from_file("debug.txt", "stem")
+				self.read_from_file(self.DEBUG_STEM, "stem")
 				self.stemming_str()
 			except:
-				print "! Error in reading from file debug.txt"
-
-		else:
+				print "! Error in reading from file debug.txt. Redo stemming"
+				debug = False
+		if (not debug):
 			for item in self.tweetarray:
 				stemmed_tweet = self.stem_tweet(item)		
 				self.stemmed_tweets_array.append(stemmed_tweet)
@@ -81,76 +98,96 @@ class Preprocessing(object):
 		
 	def tokenize(self):
 		""" Tokenize all tweets given to object and set to array """
-		if (self.debug):
+		print "** Create tokens.."
+		debug = self.debug
+		if (debug):
 			try:
-				self.read_from_file("debug.txt", "token")
+				self.read_from_file(self.DEBUG_TOKEN, "token")
 				self.stemming_str()
 			except:
-				print "! Error in reading from file debug.txt"
-		else:
+				print "! Error in reading from file debug.txt. Redo tokenization"
+				debug = False
+		if (not debug):
 			for item in self.tweetarray:
 				tokens = nltk.word_tokenize(item)
 				self.tokenized_tweets_array.append(tokens)
 
-	def frogtokens(self, frogmode):
+	def frogtokens(self):
 		""" Lematize all tweets given to object using frog server/client and set to array """
-		if (self.debug):
+		debug = self.debug
+		if (debug):
 			try:
-				self.read_from_file("debug.txt", "lemma")
-				self.stemming_str()
+				if( "lemma" in self.mode ):
+					self.read_from_file(self.DEBUG_LEMMA, frogmode)
+					self.pos_str()
+				if( "pos" in self.mode ):
+					self.read_from_file(self.DEBUG_POS, frogmode)
+					self.lemmatized_str()
 			except:
-				print "! Error in reading from file debug.txt"
-		else:
+				print "! Error in reading from file debug.txt. Redo frogtokens"
+				debug = False
+		if(not debug):
 			self.startFrogServer('start')			
 			time.sleep(15)							# Time for startup server
-			frogclient = FrogClient('localhost',self.portnumber)
+			frogclient = FrogClient('localhost',self.PORTNUMBER)
+			if( "lemma" in self.mode ):
+				print "** Creating lemma's.."
 
+			if ( "pos" in self.mode ):
+				print "** Creating POS tags.."
+			
 			for item in self.tweetarray:
-				tokens = self.frog_tweets(frogclient, item, frogmode)
 				# Write tokens to file for later testing
-				self.lemmatized_tweets_array.append(tokens)
-
+				tokensword, tokenslemma, tokenspos = self.frog_tweets(frogclient, item)
+				if ( tokenslemma ):
+					self.lemmatized_tweets_array.append(tokenslemma)
+				if ( tokenspos ):
+					self.pos_tweets_array.append(tokenspos)
 			self.startFrogServer('stop')
 
 	def startFrogServer(self, mode):
 		""" Starts/stops Frog server in seperate terminal """
 		if(mode == 'start'):
-			print "Start Frog Server"
-			os.system("mate-terminal -e 'frog -S " + str(self.portnumber) + "'")
+			print "** Start Frog Server"
+			os.system("mate-terminal -e 'frog -S " + str(self.PORTNUMBER) + "'")
 		if(mode == 'stop'):
-			print "Close Frog Server"
+			print "** Close Frog Server"
 			proc = subprocess.Popen(["pgrep", 'frog'], stdout=subprocess.PIPE) 
 			for pid in proc.stdout: 
 				os.kill(int(pid), signal.SIGTERM)
 
-	def frog_tweets(self, frogclient, tweet, frogmode):
+	def frog_tweets(self, frogclient, tweet):
 		"""	Use frog for processing according to mode. Return array of processed words """
 		frogtweet = frogclient.process(tweet.lower())
-		tokens = []
+		tokensword = []
+		tokenslemma = []
+		tokenspos = []
 		for test in frogtweet:
 			# Frog sometimes contains tuple of None
 			if (None in test):
 				pass
 			else:
 				word, lemma, morph, pos = test
-				if(frogmode == 'word'):
-					tokens.append(word)
-				if(frogmode == 'lemma'):
-						tokens.append(lemma)
-				if(frogmode == 'pos'):
-					tokens.append(pos)
-		return tokens
+				if('word' in self.mode):
+					tokensword.append(word)
+				if('lemma' in self.mode):
+					tokenslemma.append(lemma)
+				if('pos' in self.mode):
+					tokenspos.append(pos)
+		return (tokensword, tokenslemma, tokenspos)
 
-	def read_from_file(self, filename, mode):
+	def read_from_file(self, filename):
 		"""	Load array from file """
 		f = file(filename, "r")
 
 		# Load into correct array
-		if mode == "stem":
+		if ( "stem" in self.mode):
 			self.stemmed_tweets_array = pickle.load(f)
-		if mode == "token": 
+		if ( "token" in self.mode):
 			self.tokenized_tweets_array = pickle.load(f)
-		if mode == "lemma": 
+		if ( "pos" in self.mode): 
+			self.pos_tweets_array = pickle.load(f)
+		if ( "lemma" in self.mode):
 			self.lemmatized_tweets_array = pickle.load(f)
 
 	def write_to_file(self, filename, array):
