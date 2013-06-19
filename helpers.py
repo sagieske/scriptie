00356be4c7532b4b0d38f9f9a86f9c4631d437d2
sagieske <sharongieske@gmail.com>
2013-06-19 17:14:11 +0200

    pickle load function adjusted

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 # File for help functions
 import pickle
+import csv
 
 def dump_to_file(filename, array):
 	"""	Dump array to file """
@@ -15,4 +16,30 @@ def unfold_tuples(dictionary):
 	list(set(unfold_tuples_array))
 	return unfold_tuples_array
 
+def unfold_tuples_strings(array):
+	""" Unfold all tuples in dictionary to strings"""
+	unfold_tuples_strings = []
+	for tupleset in array:
+		string = ''
+		for index, word in enumerate(tupleset):
+			if index != len(tupleset)-1:
+				string += word + ' '
+			else:
+				string += word
+		unfold_tuples_strings.append(string)
+	return unfold_tuples_strings
+
+def write_to_csv(filename, write_type, row_array):
+	""" Write arrays to CSV file """
+	openfile  = open(filename, write_type)
+	csv_writer = csv.writer(openfile,delimiter = ',')
+	for row in row_array:
+		csv_writer.writerow(row)
+	openfile.close()
+
+def read_from_file(filename):
+	"""	Load array from file """
+	f = file(filename, "r")
+	pickle_object = pickle.load(f)
+	return pickle_object
 
